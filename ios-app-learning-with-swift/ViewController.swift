4be04d476cb5d2d5737fa9375c1689bc3a753c86
黄佳琪 <jiaqi228@gmail.com>
2018-05-13 00:40:27 +0800

    Initial Commit

---
+++ ios-app-learning-with-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-app-learning-with-swift
+//
+//  Created by Huang Jiaqi on 2018/5/13.
+//  Copyright © 2018年 Huang Jiaqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
